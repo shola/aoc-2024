512f7cbb5377dfc3c23f9deeb30cabc598ea0848
shola <michael.o.situ@gmail.com>
2024-12-03 15:17:40 -0800

    refactor: utils.ts - specify `ExerciseVariant` and add its type predicate function - break out CLI token parsing into `getCLITokens` function - directly access the items in `puzzle` instead of mapping over the 2-item array using a ternary - rename `createFileName` to `createInputFileName` and add jsdoc

--- utils.ts
+++ utils.ts
@@ -8,20 +8,47 @@ export async function runSolution(solution: (data: string[]) => any) {
   console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
 }
 
-export async function readData() {
-  const [_, fullPath, dataSet] = process.argv as
-    | [string, string, string]
-    | [string, string];
-  const puzzle = fullPath.split('/').slice(-2).join('/');
-  const [day, part] = puzzle
-    .split('/')
-    .map((x, i) => (i === 0 ? +x.split('-')[1] : x)) as [number, 'a' | 'b'];
-  const fileName = createFileName(day, part, dataSet);
+type ExerciseVariant = 'a' | 'b';
+function isExerciseVariant(str: string): str is ExerciseVariant {
+  return str === 'a' || str === 'b';
+}
+
+function getCLITokens(): [number, ExerciseVariant, string?] {
+  // drop the node executable path since it's not used
+  const [, fullPath, altDataSet] = process.argv as [string, string, string];
+
+  // `puzzle` will be in the form ['day-1', 'b']
+  const puzzle = fullPath.split('/').slice(-2);
+  if (!isExerciseVariant(puzzle[1])) {
+    throw new Error("Only 'a' and 'b' are allowed exercise variants");
+  }
+
+  // since the `+` precedence is lower than array access, the string gets
+  // cast to number after the numerical string has been split.
+  const [day, part] = [+puzzle[0].split('-')[1], puzzle[1]];
+
+  return [day, part, altDataSet];
+}
+
+export async function readData(): Promise<string[]> {
+  const fileName = createInputFileName(...getCLITokens());
   const data = (await readFile(fileName)).toString().split('\n');
   return data;
 }
 
-function createFileName(day: number, part: 'a' | 'b', dataSet?: string) {
-  return join(`day-${day}`, `${part}.data${dataSet ? `.${dataSet}` : ''}.txt`);
+/**
+ * Creates a standardized input filename so that exercises can be run in shorthand (see README.md)
+ * @param {string?} dataSet slug for alternative file input (in file `day-1/a.data.sample.txt`,
+ *    the slug is `sample`)
+ * @returns {string} filenames of the form `day-1/a.data.sample.txt`, where `sample` is optional
+ */
+function createInputFileName(
+  day: number,
+  part: ExerciseVariant,
+  altDataSet?: string
+) {
+  return join(
+    `day-${day}`,
+    `${part}.data${altDataSet ? `.${altDataSet}` : ''}.txt`
+  );
 }
-
