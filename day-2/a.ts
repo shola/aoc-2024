881bdb931812a42efd0096c3578b041bf74be64c
shola <michael.o.situ@gmail.com>
2024-12-03 16:53:09 -0800

    feat: completed day 2

--- day-2/a.ts
+++ day-2/a.ts
@@ -1,9 +1,77 @@
 import { runSolution } from '../utils.ts';
 
-/** provide your solution as the return of this function */
-export async function day2a(data: string[]) {
+/**
+--- Day 2: Red-Nosed Reports ---
+
+Fortunately, the first location The Historians want to search isn't a long walk from the Chief Historian's office.
+
+While the Red-Nosed Reindeer nuclear fusion/fission plant appears to contain no sign of the Chief Historian, the engineers there run up to you as soon as they see you. Apparently, they still talk about the time Rudolph was saved through molecular synthesis from a single electron.
+
+They're quick to add that - since you're already here - they'd really appreciate your help analyzing some unusual data from the Red-Nosed reactor. You turn to check if The Historians are waiting for you, but they seem to have already divided into groups that are currently searching every corner of the facility. You offer to help with the unusual data.
+
+The unusual data (your puzzle input) consists of many reports, one report per line. Each report is a list of numbers called levels that are separated by spaces. For example:
+
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+
+This example data contains six reports each containing five levels.
+
+The engineers are trying to figure out which reports are safe. The Red-Nosed reactor safety systems can only tolerate levels that are either gradually increasing or gradually decreasing. So, a report only counts as safe if both of the following are true:
+
+    The levels are either all increasing or all decreasing.
+    Any two adjacent levels differ by at least one and at most three.
+
+In the example above, the reports can be found safe or unsafe by checking those rules:
+
+    7 6 4 2 1: Safe because the levels are all decreasing by 1 or 2.
+    1 2 7 8 9: Unsafe because 2 7 is an increase of 5.
+    9 7 6 2 1: Unsafe because 6 2 is a decrease of 4.
+    1 3 2 4 5: Unsafe because 1 3 is increasing but 3 2 is decreasing.
+    8 6 4 4 1: Unsafe because 4 4 is neither an increase or a decrease.
+    1 3 6 7 9: Safe because the levels are all increasing by 1, 2, or 3.
+
+So, in this example, 2 reports are safe.
+
+Analyze the unusual data from the engineers. How many reports are safe?
+
+ */
+
+/**
+ * This puzzle can be thought of as a filter operation on the lines of input.
+ * The filter function will return true if:
+ * - successive numbers are either increasing or decreasing
+ * - successive numbers differ by at least 1 and at most 3
+ */
+export async function day2a(data: string[]): Promise<number> {
   console.log(data);
-  return 0;
+  let numSafeReports = 0;
+
+  const reports: number[][] = data.map((line: string) =>
+    line.split(' ').map(Number)
+  );
+
+  reports.forEach((report) => {
+    // NOTE: this does not differentiate between consecutive descending or equal values
+    const isAscending = report[0] < report[1];
+
+    // since each iteration checks `i` against `i+1`, exit before the last element
+    for (let i = 0; i < report.length - 1; i++) {
+      const [a, b] = [report[i], report[i + 1]];
+      const diff = Math.abs(a - b);
+
+      // Check for invalid diff and exit early. This handles the consecutive equal values
+      // case with `diff === 0`, and must be checked before the direction.
+      if (diff === 0 || diff > 3 || a < b !== isAscending) return;
+    }
+
+    numSafeReports++;
+  });
+
+  return numSafeReports;
 }
 
 await runSolution(day2a);
--- day-2/b.ts
+++ day-2/b.ts
@@ -1,9 +1,89 @@
 import { runSolution } from '../utils.ts';
 
+/**
+--- Part Two ---
+
+The engineers are surprised by the low number of safe reports until they realize they forgot to tell you about the Problem Dampener.
+
+The Problem Dampener is a reactor-mounted module that lets the reactor safety systems tolerate a single bad level in what would otherwise be a safe report. It's like the bad level never happened!
+
+Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
+
+More of the above example's reports are now safe:
+
+    7 6 4 2 1: Safe without removing any level.
+    1 2 7 8 9: Unsafe regardless of which level is removed.
+    9 7 6 2 1: Unsafe regardless of which level is removed.
+    1 3 2 4 5: Safe by removing the second level, 3.
+    8 6 4 4 1: Safe by removing the third level, 4.
+    1 3 6 7 9: Safe without removing any level.
+
+Thanks to the Problem Dampener, 4 reports are actually safe!
+
+Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. How many reports are now safe?
+*/
+
 /** provide your solution as the return of this function */
 export async function day2b(data: string[]) {
   console.log(data);
-  return 0;
+
+  let numSafeReports = 0;
+
+  const reports: number[][] = data.map((line: string) =>
+    line.split(' ').map(Number)
+  );
+
+  /**
+   * Convert each report into a list of consecutive diffs and mark as safe if:
+   * - a valid report would be one where all values are negative, or all are positive
+   * - 0 < diff <= 3 for all values
+   * If either condition fails, remove each item in the report and try again
+   */
+
+  reports.forEach((report) => {
+    // This if-else could be collapsed into a single if, but debugging would be harder
+    if (hasValidDiffedReport(report)) {
+      numSafeReports++;
+    } else if (isValidReportWithSingleItemRemoved(report)) {
+      numSafeReports++;
+    }
+  });
+
+  return numSafeReports;
+}
+
+function convertReportToDiffLists(report: number[]) {
+  const diffs = [];
+
+  for (let i = 0; i < report.length - 1; i++) {
+    diffs.push(report[i] - report[i + 1]);
+  }
+
+  return diffs;
+}
+
+function hasValidDiffedReport(report: number[]) {
+  const diffs = convertReportToDiffLists(report);
+  const allSameDirection = diffs.every((diff) => diff > 0) || diffs.every((diff) => diff < 0);
+  const allInBounds = diffs.every((diff) => Math.abs(diff) > 0 && Math.abs(diff) <= 3)
+
+  return allSameDirection && allInBounds;
+}
+
+/**
+ * Retry the diff checking while removing each item in the report.
+ * It would be more efficient to find the first item that fails, and
+ * check from there, but this algorithm handles each report as a whole
+ * instead of stepping through each item.
+ */ 
+function isValidReportWithSingleItemRemoved(report: number[]) {
+  for (let i = 0; i < report.length; i++) {
+    const reportWithIRemoved = [...report.slice(0, i), ...report.slice(i + 1)];
+
+    if (hasValidDiffedReport(reportWithIRemoved)) return true;
+  }
+
+  return false;
 }
 
 await runSolution(day2b);
