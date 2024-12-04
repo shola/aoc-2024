4c73f5f3a381bb00f5419ef219d8e8c1f0de8e39
shola <michael.o.situ@gmail.com>
2024-12-04 00:45:45 -0800

    feat: completed day 4

--- day-4/a.ts
+++ day-4/a.ts
@@ -1,9 +1,120 @@
 import { runSolution } from '../utils.ts';
 
+/**
+--- Day 4: Ceres Search ---
+
+"Looks like the Chief's not here. Next!" One of The Historians pulls out a device and pushes the only button on it. After a brief flash, you recognize the interior of the Ceres monitoring station!
+
+As the search for the Chief continues, a small Elf who lives on the station tugs on your shirt; she'd like to know if you could help her with her word search (your puzzle input). She only has to find one word: XMAS.
+
+This word search allows words to be horizontal, vertical, diagonal, written backwards, or even overlapping other words. It's a little unusual, though, as you don't merely need to find one instance of XMAS - you need to find all of them. Here are a few ways XMAS might appear, where irrelevant characters have been replaced with .:
+
+..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X....
+
+The actual word search will be full of letters instead. For example:
+
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+
+In this word search, XMAS occurs a total of 18 times; here's the same word search again, but where letters not involved in any XMAS have been replaced with .:
+
+....XXMAS.
+.SAMXMS...
+...S..A...
+..A.A.MS.X
+XMASAMX.MM
+X.....XA.A
+S.S.S.S.SS
+.A.A.A.A.A
+..M.M.M.MM
+.X.X.XMASX
+
+Take a look at the little Elf's word search. How many times does XMAS appear?
+
+ */
 /** provide your solution as the return of this function */
 export async function day4a(data: string[]) {
-  console.log(data);
-  return 0;
+
+  const crosswordBoard = data.map((line) => line.split(''));
+  let xmasCount = 0;
+
+  // Check for 'XMAS' or 'SAMX' down, right, diagonalDownRight,
+  // diagonalDownLeft. No need to check up or left because they
+  // will already be checked by previous iterations.
+  for (let r = 0; r < crosswordBoard.length; r++) {
+    for (let c = 0; c < crosswordBoard[r].length; c++) {
+      if (checkDown(crosswordBoard, r, c)) xmasCount++;
+      if (checkRight(crosswordBoard, r, c)) xmasCount++;
+      if (checkDiagonalDownRight(crosswordBoard, r, c)) xmasCount++;
+      if (checkDiagonalDownLeft(crosswordBoard, r, c)) xmasCount++;
+    }
+  }
+
+  return xmasCount;
+}
+
+function checkDown(crosswordBoard, r, c): boolean {
+  // out of bounds
+  if (r + 4 > crosswordBoard.length) return false;
+
+  const word = [
+    crosswordBoard[r][c],
+    crosswordBoard[r + 1][c],
+    crosswordBoard[r + 2][c],
+    crosswordBoard[r + 3][c],
+  ].join('');
+  return word === 'XMAS' || word === 'SAMX';
+}
+
+function checkRight(crosswordBoard, r, c): boolean {
+  // out of bounds
+  if (c + 4 > crosswordBoard[r].length) return false;
+
+  const word = [
+    crosswordBoard[r][c],
+    crosswordBoard[r][c + 1],
+    crosswordBoard[r][c + 2],
+    crosswordBoard[r][c + 3],
+  ].join('');
+  return word === 'XMAS' || word === 'SAMX';
+}
+
+function checkDiagonalDownRight(crosswordBoard, r, c): boolean {
+  // out of bounds
+  if (r + 4 > crosswordBoard.length || c + 4 > crosswordBoard[r].length) return false;
+
+  const word = [
+    crosswordBoard[r][c],
+    crosswordBoard[r + 1][c + 1],
+    crosswordBoard[r + 2][c + 2],
+    crosswordBoard[r + 3][c + 3],
+  ].join('');
+  return word === 'XMAS' || word === 'SAMX';
+}
+
+function checkDiagonalDownLeft(crosswordBoard, r, c): boolean {
+  // out of bounds
+  if (r + 4 > crosswordBoard.length || c - 3 < 0) return false;
+
+  const word = [
+    crosswordBoard[r][c],
+    crosswordBoard[r + 1][c - 1],
+    crosswordBoard[r + 2][c - 2],
+    crosswordBoard[r + 3][c - 3],
+  ].join('');
+  return word === 'XMAS' || word === 'SAMX';
 }
 
 await runSolution(day4a);
--- day-4/b.ts
+++ day-4/b.ts
@@ -1,9 +1,69 @@
 import { runSolution } from '../utils.ts';
+/**
+--- Part Two ---
 
+The Elf looks quizzically at you. Did you misunderstand the assignment?
+
+Looking for the instructions, you flip over the word search to find that this isn't actually an XMAS puzzle; it's an X-MAS puzzle in which you're supposed to find two MAS in the shape of an X. One way to achieve that is like this:
+
+M.S
+.A.
+M.S
+
+Irrelevant characters have again been replaced with . in the above diagram. Within the X, each MAS can be written forwards or backwards.
+
+Here's the same example from before, but this time all of the X-MASes have been kept instead:
+
+.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........
+
+In this example, an X-MAS appears 9 times.
+
+Flip the word search from the instructions back over to the word search side and try again. How many times does an X-MAS appear?
+
+ */
 /** provide your solution as the return of this function */
 export async function day4b(data: string[]) {
-  console.log(data);
-  return 0;
+  const crosswordBoard = data.map((line) => line.split(''));
+  let xmasCount = 0;
+
+  for (let r = 0; r < crosswordBoard.length - 2; r++) {
+    for (let c = 0; c < crosswordBoard[r].length - 2; c++) {
+      if (checkX(crosswordBoard, r, c)) xmasCount++;
+    }
+  }
+
+  return xmasCount;
+}
+
+function checkX(crosswordBoard, r, c): boolean {
+  // out of bounds
+  if (r + 3 > crosswordBoard.length || c + 3 > crosswordBoard[r].length)
+    return false;
+
+  const downRight = [
+    crosswordBoard[r + 0][c],
+    crosswordBoard[r + 1][c + 1],
+    crosswordBoard[r + 2][c + 2],
+  ].join('');
+  const upRight = [
+    crosswordBoard[r + 2][c],
+    crosswordBoard[r + 1][c + 1],
+    crosswordBoard[r + 0][c + 2],
+  ].join('');
+
+  return (
+    (downRight === 'MAS' || downRight === 'SAM') &&
+    (upRight === 'MAS' || upRight === 'SAM')
+  );
 }
 
 await runSolution(day4b);
