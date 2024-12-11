fe40a6426b19d051d76ab208901199e5802a3294
shola <michael.o.situ@gmail.com>
2024-12-11 05:01:26 -0800

    feat: day 7

--- day-7/a.ts
+++ day-7/a.ts
@@ -3,7 +3,31 @@ import { runSolution } from '../utils.ts';
 /** provide your solution as the return of this function */
 export async function day7a(data: string[]) {
   console.log(data);
-  return 0;
+
+  let sum = 0;
+
+  data.forEach(line => {
+    const [rawAnswer, rawList] = line.split(': ');
+    const answer = +rawAnswer;
+    const list = rawList.split(' ').map(Number);
+
+    if (solver(list, answer)) sum += answer;
+  });
+
+  return sum;
+}
+
+// DFS trying addition first
+function solver(list: number[], answer: number, intermediate?: number) {
+  if (list.length === 0) return intermediate === answer;
+
+  const head = list[0];
+  const rest = list.slice(1);
+
+  const added = intermediate ? intermediate + head : head;
+  const multiplied = intermediate ? intermediate * head : head;
+
+  return solver(rest, answer, added) || solver(rest, answer, multiplied);
 }
 
 await runSolution(day7a);
--- day-7/b.ts
+++ day-7/b.ts
@@ -3,7 +3,32 @@ import { runSolution } from '../utils.ts';
 /** provide your solution as the return of this function */
 export async function day7b(data: string[]) {
   console.log(data);
-  return 0;
+
+  let sum = 0;
+
+  data.forEach(line => {
+    const [rawAnswer, rawList] = line.split(': ');
+    const answer = +rawAnswer;
+    const list = rawList.split(' ').map(Number);
+
+    if (solver(list, answer)) sum += answer;
+  });
+
+  return sum;
+}
+
+// DFS trying addition first
+function solver(list: number[], answer: number, intermediate?: number) {
+  if (list.length === 0) return intermediate === answer;
+
+  const head = list[0];
+  const rest = list.slice(1);
+
+  const added = intermediate ? intermediate + head : head;
+  const multiplied = intermediate ? intermediate * head : head;
+  const concatenated = intermediate ? +`${intermediate}${head}` : head;
+
+  return solver(rest, answer, added) || solver(rest, answer, multiplied) || solver(rest, answer, concatenated);
 }
 
 await runSolution(day7b);
